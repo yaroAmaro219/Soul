976c50e1b2b10e586e26aa0ca02cc006aec23e81
Artemie Amari <yaroamari@gmail.com>
2020-08-07 15:36:07 -0400

    manifest.json

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Soul Deep Soul</h1>
+      <h4>A blog focused on what we know about the Soul connection to the mind and body</h4>
     </div>
   );
 }
